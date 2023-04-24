aaf5bce8b8ac760d97b595b325e6bb175f8cbe4a
nathank07 <nathank7256@gmail.com>
2023-04-24 02:57:33 -0500

    Added sumDict matcher test and whiteChessPawn test

--- sum.js
+++ sum.js
@@ -3,4 +3,17 @@ function sum(a, b){
     return a + b;
 }
 
-module.exports = sum
+function sumDict(a, b){
+    const dict = {'first number': a}
+    dict['second number'] = b
+    dict['third number'] = a + b
+    return dict
+}
+
+function whiteChessPawn(oldSquare, newSquare) {
+    if(oldSquare[0] === newSquare[0]){
+        const squaresMoved = oldSquare[1] + newSquare[2];
+        return squaresMoved === 1 || squaresMoved === 2
+    }
+}
+module.exports = {sum, sumDict, whiteChessPawn}
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,17 @@
-const sum = require('./sum')
+const {sum, sumDict, whiteChessPawn} = require('./sum')
 
 test('Addition', () => {
     expect(sum(1,2)).toBe(3);
 });
+
+test('object assignment', () => {
+    expect(sumDict(1,2)).toEqual({'first number' : 1, "second number" : 2, "third number" : 3})
+});
+
+test('White chess pawn can move forward', () => {
+    for(i = 0, i < 2; i++;) {
+        for(j = 0, i < 8; i++;){
+            expect(whiteChessPawn([j, 2], [j, 2 + i])).toBeTruthy()
+        }
+    }
+})
