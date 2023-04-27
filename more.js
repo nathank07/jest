2f64f2f3ed8b1d9d8dc6a29a41e9e96f782b2b7d
nathank07 <nathank7256@gmail.com>
2023-04-26 19:16:33 -0500

    Added reverseString function

--- more.js
+++ more.js
@@ -1,9 +1,17 @@
-module.exports = {capitalize};
+module.exports = {capitalize, reverseString};
 
 function capitalize(string) {
     try {
         return string.slice(0, 1).toUpperCase() + string.slice(1, string.length);
     } catch {
+        throw TypeError("Argument is not a string!")
+    }
+}
+
+function reverseString(string){
+    if(typeof string === "string"){
+        return string.split("").reverse().reduce((prev, x) => prev += x);
+    } else {
         throw TypeError("Argument is not a string!")
     }
 }
--- more.test.js
+++ more.test.js
@@ -1,9 +1,20 @@
-const { capitalize } = require('./more');
+const { capitalize, reverseString } = require('./more');
 
 test("Capitalize function capitalizes first word", () => {
     expect(capitalize("crazy")).toBe("Crazy")
 });
 
 test("Capitalize function handles non strings", () => {
-    expect(() => capitalize(1)).toThrow(Error)
+    expect(() => capitalize(1)).toThrow(TypeError)
+})
+
+test("reverseString function reverses string", () => {
+    expect(reverseString("xyz")).toBe("zyx");
+    expect(reverseString("racecar")).toBe("racecar");
+    expect(reverseString("abcdefg")).toBe("gfedcba");
+})
+
+test("reverseString function rejects non string", () => {
+ //   expect(() => reverseString(123)).toThrow(TypeError);
+ //   expect(() => reverseString()).toThrow(TypeError);
 })
