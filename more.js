7220155155e80b3d0af2553de485eff4896cff0b
nathank07 <nathank7256@gmail.com>
2023-04-26 19:59:39 -0500

    Added calculator object with tests

--- more.js
+++ more.js
@@ -1,5 +1,3 @@
-module.exports = {capitalize, reverseString};
-
 function capitalize(string) {
     try {
         return string.slice(0, 1).toUpperCase() + string.slice(1, string.length);
@@ -15,3 +13,32 @@ function reverseString(string){
         throw TypeError("Argument is not a string!")
     }
 }
+
+const calculator = {
+    checkTypes(num1, num2){
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            throw new TypeError('Both arguments must be numbers');
+        }
+    },
+    sum(num1, num2){
+        this.checkTypes(num1, num2);
+        return Number((num1 + num2).toFixed(10));
+    },
+    subtract(num1, num2){
+        this.checkTypes(num1, num2);
+        return Number((num1 - num2).toFixed(10));
+    },
+    multiply(num1, num2){
+        this.checkTypes(num1, num2);
+        return Number((num1 * num2).toFixed(10));
+    },
+    divide(num1, num2){
+        this.checkTypes(num1, num2);
+        if(num2 === 0){
+            throw new Error('Cannot divide by 0')
+        }
+        return Number((num1 / num2).toFixed(10));
+    },
+}
+
+module.exports = { capitalize, reverseString, calculator };
--- more.test.js
+++ more.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require('./more');
+const { capitalize, reverseString, calculator } = require('./more');
 
 test("Capitalize function capitalizes first word", () => {
     expect(capitalize("crazy")).toBe("Crazy")
@@ -17,4 +17,39 @@ test("reverseString function reverses string", () => {
 test("reverseString function rejects non string", () => {
     expect(() => reverseString(123)).toThrow(TypeError);
     expect(() => reverseString()).toThrow(TypeError);
+})
+
+test("Calculator adds properly", () => {
+    expect(calculator.sum(1,2)).toBe(3);
+    expect(calculator.sum(1.2,2.5)).toBe(3.7);
+    expect(calculator.sum(0.1,0.2)).toBe(0.3);
+})
+
+test("Calculator subtracts properly", () => {
+    expect(calculator.subtract(1,2)).toBe(-1);
+    expect(calculator.subtract(1.2,2.5)).toBe(-1.3);
+    expect(calculator.subtract(0.1,0.2)).toBe(-0.1);
+    expect(calculator.subtract(782,100)).toBe(682);
+})
+
+test("Calculator multiplies properly", () => {
+    expect(calculator.multiply(1,2)).toBe(2);
+    expect(calculator.multiply(1.2,2.5)).toBe(3);
+    expect(calculator.multiply(0.1,0.2)).toBe(0.02);
+    expect(calculator.multiply(782,100)).toBe(78200);
+})
+
+test("Calculator divides properly", () => {
+    expect(calculator.divide(1,2)).toBe(0.5);
+    expect(calculator.divide(1.2,2.5)).toBe(0.48);
+    expect(calculator.divide(0.1,0.2)).toBe(0.5);
+    expect(calculator.divide(782,100)).toBe(7.82);
+    expect(() => calculator.divide(1,0)).toThrow(Error);
+})
+
+test("Calculator rejects non numbers", () => {
+    expect(() => calculator.sum("x", "y")).toThrow(TypeError);
+    expect(() => calculator.subtract()).toThrow(TypeError);
+    expect(() => calculator.multiply("1", "3")).toThrow(TypeError);
+    expect(() => calculator.divide("4")).toThrow(TypeError);
 })
